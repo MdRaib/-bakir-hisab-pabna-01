2b6390ad85050f32d9b5516cea6dd9ab3c2f34bd
MdRaib <91070747+MdRaib@users.noreply.github.com>
2026-09-20 02:12:30 +0600

    Update config.js

--- config.js
+++ config.js
@@ -10,7 +10,7 @@ var APP_CONFIG = Object.freeze({
   CLIENT_ID: "822363790054-s0n5f46cfj134jgpcog4114jo9qf8fb9.apps.googleusercontent.com",
 
   // Deployed Google Apps Script Web App URL for the central license sheet.
-  ADMIN_API_URL: "Https://script.google.com/macros/s/AKfycbwhTmvkeL23OkGjzcFLx7jLpdjS_eXS1ENZp54-0Fw48lQ4m0IqPQHTzTZBlqDGaWi4/exec",
+  ADMIN_API_URL: "https://script.google.com/macros/s/AKfycbx4QyzlBqqyKActc3g-P9FkmoTSfZLVA418iSXa0x7VS3RMylbT4u9Vh55kk6lmRn6Q/exec",
 
   // MUST be unique for every deployed client instance.
   SITE_ID: "client_pabna_01",
